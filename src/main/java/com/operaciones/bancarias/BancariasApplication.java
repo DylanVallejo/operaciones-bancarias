b2e93c0c4b9cd18abb3c4d237500a43886feaf83
DylanVallejo <dylan1811vallejo@gmail.com>
2024-02-20 11:41:16 -0500

    - first commit proyecto cuentas bancarias, estructura del proyecto creada y configuracion a base de datos lista

---
+++ src/main/java/com/operaciones/bancarias/BancariasApplication.java
@@ -0,0 +1,13 @@
+package com.operaciones.bancarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/operaciones/bancarias/BancariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operaciones.bancarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
